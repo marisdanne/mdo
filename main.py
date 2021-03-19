4085ade35ff328cde7163ec6e4ee39a63729f806
AiritaSh <ashuksto@inbox.lv>
2021-03-19 13:56:10 +0200

    has ar login

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, jsonify, request, redirect
+import hashlib
 import json
 import sqlite3
 from darbs_ar_failu import nolasitDatus, ierakstitDatus
 from darbs_ar_db import registret, atlasit, lietotaji, pieteikties, atlasit_lietotajus, atlasit_prasmes, pievienot_prasmi
 
 app = Flask(__name__)
-
+SALT = 'm@d@o'
 
 @app.route('/')
 def home():
@@ -45,36 +46,15 @@ def login():
     else:
         lietotajvards = request.form.get('lietotajvards')
         parole = request.form.get('parole')
-        loma = pieteikties(lietotajvards, parole)
-        print (loma)
+        parole_hash = hashlib.md5(bytes(parole + SALT, 'utf-8'))
+        parole_encode = parole_hash.hexdigest()
+        loma = pieteikties(lietotajvards, parole_encode)
         if loma == "Skolotājs":
             return redirect('/izvelneskolotajs')
         elif loma == "Skolēns":
             return redirect('/izvelneskolens')
         else:
             return render_template('login.html')
-
-
-
-
-    #salt = 'm@d@o'
-#    parole = "skola"
-   
-#    parole_hash = hashlib.md5(bytes(parole, 'utf-8'))
-#    parole_encode = parole_hash.hexdigest()
-#    paroles_ievads = input('Ievadi paroli: ')
-#    paroles_ievads_hash = hashlib.md5(bytes(paroles_ievads, 'utf-8'))
-#    print(paroles_ievads_hash)
-#    paroles_ievads_encode = paroles_ievads_hash.hexdigest()
-#    print(paroles_ievads_encode)
-#    if parole_encode == paroles_ievads_encode:
-#       print("Parole ievadīta pareizi")
-#    else:
-#      print("Parole ievadīta nepareizi")
-#    return "0"
- 
-
-    
 
 @app.route('/izvelneskolens')
 def izvelneskolens():
